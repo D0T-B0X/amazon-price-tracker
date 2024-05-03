83e42e356f38a71065128b9f89e94f783dae9898
Dot-Box <91383632+D0T-B0X@users.noreply.github.com>
2024-05-04 00:53:13 +0530

    Update main.py
    had to hide some sensitive info

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from smtplib import SMTP
 load_dotenv()
 SENDERS_EMAIL = os.getenv("MY_EMAIL")
 RECEIVERS_EMAIL = os.getenv("RECEIVERS_EMAIL")
-NEEDED_PRICE = 35000
+NEEDED_PRICE = <amount>
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:125.0) Gecko/20100101 Firefox/125.0",
@@ -16,7 +16,7 @@ headers = {
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8"
 }
 
-source = requests.get(url="https://www.amazon.in/SAMSUNG-Galaxy-S23-Purple-Storage/dp/B0CJXQV9R2/", headers=headers)
+source = requests.get(url="<amazon url of needed item>", headers=headers)
 
 soup = BeautifulSoup(source.text, "lxml")
 price = int((soup.find("span", class_="a-price-whole").getText()[:-1]).replace(',', ''))
@@ -24,11 +24,11 @@ price = int((soup.find("span", class_="a-price-whole").getText()[:-1]).replace('
 if price < NEEDED_PRICE:
     with SMTP("smtp-mail.outlook.com", 587) as connection:
         connection.starttls()
-        connection.login(user="pythontest122@outlook.com", password="knsp2793")
+        connection.login(user=SENDERS_EMAIL, password="knsp2793") #I use outlook accounts since they are easier to work with, use whatever you like and configure the smtp host accordingly
         connection.sendmail(
             from_addr=SENDERS_EMAIL,
             to_addrs=RECEIVERS_EMAIL,
             msg="Subject: The price has dropped!"
-                f"\n\nThe price of the S23 FE has dropped below {NEEDED_PRICE} to {price} \n"
-                "Buy it here: https://www.amazon.in/SAMSUNG-Galaxy-S23-Purple-Storage/dp/B0CJXQV9R2/"
+                f"\n\nThe price of the <item> has dropped below {NEEDED_PRICE} to {price} \n"
+                "Buy it here: <product link>"
         )
